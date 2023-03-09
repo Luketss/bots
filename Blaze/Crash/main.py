b8e73d77565940f3af7739f5cccb8d3655989654
Lucas Silluzio <lucassilluzio@gmail.com>
2023-03-09 19:56:54 -0300

    blaze bot double/crash

---
+++ Blaze/Crash/main.py
@@ -0,0 +1,26 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+valores_passados = []
+
+driver = webdriver.Chrome()
+driver.get("https://blaze.com/pt/games/crash")
+
+time.sleep(4)
+
+previous = driver.find_element(By.XPATH, "//div[@class='crash-previous']")
+
+p = previous.find_element(By.XPATH, "//div[@class='entries']")
+
+values = p.find_elements(By.TAG_NAME, "span")
+
+for v in values:
+    if v.text != "":
+        valores_passados.append(v.text)
+
+print(valores_passados[::-1])
+
+driver.quit()
---
+++ Blaze/Double/main.py
@@ -0,0 +1,51 @@
+import time
+import json
+from datetime import datetime
+
+import requests
+
+
+class Double:
+    def __init__(
+        self,
+        min_pages=2,
+        start_date="2023-02-07",
+        end_date="2023-03-09",
+        save_file=False,
+    ):
+        self.min_pages = min_pages
+        self.start_date = start_date
+        self.end_date = end_date
+        self.cur_hour = self.get_current_time_hours()
+        self.url = f"https://blaze.com/api/roulette_games/history?startDate={self.start_date}T{self.cur_hour}.369Z&endDate={self.end_date}T{self.cur_hour}.370Z&page=1"
+        self.save_file = save_file
+        self.data = self.get_blaze_data()
+
+    def get_current_time_hours(self):
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        return current_time
+
+    def get_blaze_data(self):
+        r = requests.get(self.url)
+
+        if self.save_file:
+            self.save_data_to_file(r.text)
+        return json.loads(r.text)
+
+    def save_data_to_file(self, data):
+        with open("result.json", "w") as f:
+            f.write(data)
+
+    def get_only_result_data(self):
+        results = []
+        print(self.data)
+        for v in self.data["records"]:
+            val = v["roll"]
+            results.append(val)
+        print(results)
+
+
+if __name__ == "__main__":
+    obj = Double(save_file=True)
+    obj.get_only_result_data()
